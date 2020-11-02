1d52a72a515137ccc6f0ab5e379974a88fbe3416
Fermin Martin <fermartorr01@gmail.com>
2020-11-02 19:15:03 +0100

    MATCHERS: 7 Monitoreo

--- __test__/matchers.test.js
+++ __test__/matchers.test.js
@@ -4,8 +4,8 @@ describe('Comparadores comunes', () => {
         lastname: "barajas"
     }
     const user2 = {
-        name: "oscar2",
-        lastname: "barajas2"
+        name: "oscar",
+        lastname: "barajas"
     }
     test('igualdad de elementos', () => {
         expect(user).toEqual(user2);
